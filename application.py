7bf6a8b6419627c9c525d694fc6e284e5a558aa2
Shree <shreeraj99k@gmail.com>
2025-01-10 20:18:46 +0530

    pkl changes

--- application.py
+++ application.py
@@ -36,10 +36,10 @@ def predict_datapoint():
 
         predict_pipeline=PredictPipeline()
         print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
+        results=predict_pipeline.predict(pred_df).round(2)
         print("after Prediction")
         return render_template('home.html',results=results[0])
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
